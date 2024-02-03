eeb4661cc8c387a90d116d2dea01b07769284159
avzer0 <anavasconcelos1995@gmail.com>
2024-02-03 22:33:14 +0000

    Commit 0.4

--- script.js
+++ script.js
@@ -1,9 +1,58 @@
 const generateForm = document.querySelector(".create-form");
 const ImageGallery = document.querySelector(".image-gallery");
 
-//const generateAIImages = (userPrompt, userImgQuantity) => {
-//};
-//21.54
+const OPENAI_API_KEY = "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";
+
+const updateImageCard = (imagDataArray) => {
+    imagDataArray.forEach((imgObject, index) => {
+        const imgCard = ImageGallery.querySelectorAll(".image-card")[index];
+        const imgElement = imgCard.querySelector("img");
+        const downloadBtn = imgCard.querySelector(".download-btn");
+
+        //Set the Ai generated images has the source
+        const aiGeneratedImg = `data:image/jpeg;base64,${imgObject.b64_json}`
+        imgElement.src = aiGeneratedImg;
+        
+        //When the image is loaded, remove the loading class
+        imgElement.onload = () => {
+            imgCard.classList.remove("loading");
+            //Download image has jpg
+            downloadBtn.setAttribute("href", aiGeneratedImg);
+            downloadBtn.setAttribute("download", `${new Date ().getTime()}.jpg`);
+        };
+
+    });
+};
+
+//Use open API AI to generate the images
+const generateAIImages = async (userPrompt, userImgQuantity) => {
+    try{
+        const response = await fetch("https://api.openai.com/v1/images/generations", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${OPENAI_API_KEY}`
+            },
+            body: JSON.stringify({
+                prompt: userPrompt,
+                n: parseInt(userImgQuantity),
+                size: "512x512", //1024x1024
+                response_formate:  "b64_json"
+            })
+        });
+
+        if(!response.ok) throw new Error("Failed to generate images.")
+
+        // Collect data from response
+        const { data } = await response.json();
+        //Upadate the image gallery with the AI generated images
+        updateImageCard([...data])
+
+    } catch (error) {
+        alert(error.message)
+    }
+};
+
 
 
 
